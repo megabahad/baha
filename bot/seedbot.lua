d5a791aa3f7a31687cc982f0dfdf5669fd0d2fa4
megabaha <amirbahadoramirjanati@gmail.com>
2017-01-11 22:52:46 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,7 +224,8 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+    "admin",
+    "sudo"
     },
     sudo_users = {110626080,103649648,143723991,111020322,292114768,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
